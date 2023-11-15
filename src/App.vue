a128dab376a4aa1ddf5aa2138a1acb5fb78d9e7d
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-11-15 11:00:56 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 
-import { computed } from '@vue/reactivity';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
+// Número máximo de intentos
+const MAX_ATTEMPS = 3
 
 // Número a adivinar
 const randomNumber = ref(parseInt(Math.random() * 100) + 1)
@@ -17,11 +19,20 @@ const previousGuesses = ref([])
 const currentGuess = ref("")
 
 // número de intentos
-const attempts = ref(10)
+const attempts = ref(MAX_ATTEMPS)
 
 const startGame = () => {
   console.log("Game Starts")
   isPlaying.value = true
+}
+
+const newGame = () => {
+  console.log("Game restarts");
+  randomNumber.value = Math.floor(Math.random() * 100) + 1
+  previousGuesses.value = []
+  attempts.value = MAX_ATTEMPS
+  isPlaying.value = false
+
 }
 
 const checkGuess = () => {
@@ -33,6 +44,10 @@ const checkGuess = () => {
 
 const lastNumber = computed(() => {
   return previousGuesses.value[previousGuesses.value.length - 1]
+})
+
+const isGameFinished = computed(() => {
+  return attempts.value == 0 || lastNumber.value == randomNumber.value
 })
 // En los ficheros SFC, NO hace falta exportar ninguna variable ni función. Están automáticamente disponibles en todo el <template>
 
@@ -66,15 +81,40 @@ const lastNumber = computed(() => {
  * Corregir: 10.11
  */
 
- /**
-  * Reto: En el <template>
-  * 
-  * 1. En la <p>           <p class="lowOrHi"></p>,
-  * usa adecuadamente las directivas v-if , v-else para informar al usuario si el último número que ha introducido es mayor o menor
-  * 2. Recuerda como se usa v-if, v-else https://vuejs.org/guide/essentials/conditional.html
-  * 
-  * Corregir: 10:50
-  */
+/**
+ * Reto: En el <template>
+ * 
+ * 1. En la <p>           <p class="lowOrHi"></p>,
+ * usa adecuadamente las directivas v-if , v-else para informar al usuario si el último número que ha introducido es mayor o menor
+ * 2. Recuerda como se usa v-if, v-else https://vuejs.org/guide/essentials/conditional.html
+ * 
+ * Corregir: 10:50
+ */
+
+/**
+ * REto: mensajes de has ganado o has perdido
+ * 
+ * 1. ¿Cómo sabemos que hemos perdido? Cuando attempts es 0 Y mi ultimo intento también ha fallado
+ *   - mi ultimo intento: lastNumber
+ *   - el numero a adivinar: randomNumber
+ * 2. ¿Cómo sabemos que he ganado? Si mi último intento (lastNumber) es igual al número a adivinar, he ganado
+ * 
+ * Corregir: 9.30
+ */
+
+/**
+ * Reto: Reset game
+ * 
+ * 1. Cuando has ganado o has perdido, aparece un nuevo <button>
+ * 2. Al hacer click en el button:
+ *    1. Calcular un nuevo valor aleatorio y asignarlo a la variable random Number
+ *    2. Limpiar/Vaciar/reiniciar el array de previousGuesses
+ *    3. Cambiar el valor de la variable isPlaying a false
+ *    4. Actualizar la variable attempts
+ * 
+ * Corregir: 10.25
+ * 
+ */
 
 </script>
 
@@ -84,21 +124,25 @@ const lastNumber = computed(() => {
 
     <section v-if="isPlaying">
       <p>Try and guess a random number between 1 and 100.</p>
-      <p>You have 10 attempts to guess the right number.</p>
+      <p>You have {{MAX_ATTEMPS}} attempts to guess the right number.</p>
       <div id="wrapper">
         <label for="guessField">Guess a number</label>
-        <input v-model="currentGuess" type="number" id="guessField">
-        <button @click="checkGuess" class="button-check">Check Guess</button>
+        <input :disabled="isGameFinished" v-model="currentGuess" type="number" id="guessField">
+        <button :disabled="!currentGuess" @click="checkGuess" class="button-check">Check Guess</button>
 
         <div v-show="lastNumber" class="resultParas">
           <p>Previous Guesses: <span class="guesses"> {{ previousGuesses.join("-") }}</span></p>
           <p>Last number checked: {{ lastNumber }}</p>
           <p>Guesses Remaining: <span class="lastResult">{{ attempts }}</span></p>
-          <p v-if="randomNumber > lastNumber" style="color: green;">El numero debe ser mayor</p>
-          <p v-else style="color: red;">El numero debe ser menor</p>
-
+          <p style="color: red; background-color: black;" v-if="attempts === 0 && lastNumber != randomNumber">¡Has
+            perdido! El número a adivinar era : {{ randomNumber
+            }} </p>
+          <p v-else-if="randomNumber == lastNumber">¡Felicidades! Has ganado</p>
+          <p v-else-if="randomNumber > lastNumber" style="color: green">El número debe ser mayor</p>
+          <p v-else style="color: red">El número debe ser menor</p>
         </div>
       </div>
+      <button v-show="isGameFinished" @click="newGame" style="background-color:darkcyan">New Game</button>
     </section>
     <section v-else>
       <button @click="startGame">Start Game</button>
